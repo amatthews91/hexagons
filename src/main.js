8328c7f45da2ccb97476a0d5aa878a7eeb2d628a
Ashley <ashleymatthews91@gmail.com>
2018-11-15 17:50:35 +0000

    Adds method to tile to fetch surrounding hexes (tiles), calls this method on mouseover of the starting tile

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import * as PIXI from 'pixi.js';
 
+import Tile from './tile';
+
 const width = 512;
 const height = 512;
 const app = new PIXI.Application({ width, height });
@@ -7,51 +9,25 @@ const renderer = app.renderer;
 const stage = app.stage;
 document.body.appendChild(app.view);
 
+const startingTile = Tile.build(width / 2, height / 2, 20);
+stage.addChild(startingTile.shape);
 
-// Draws a hexagon where (x, y) are the coordinates of the centre
-// r is the radius of the hexagon.
-// returns: shape Polygon object
-const getHex = (x, y, r, renderer, stage) => {
-  //60 degrees in radians = 1.04718
-  const offsetX = r * Math.cos(1.04718);
-  const offsetY = r * Math.sin(1.04718);
+startingTile.shape.mouseover = () => {
+  startingTile.shape.clear();
+  startingTile.shape.beginFill(0x00FFFF);
+  startingTile.shape.lineStyle(1, 0xFFFFFF)
+  startingTile.shape.drawPolygon(startingTile.points);
 
-  const points = [
-    new PIXI.Point(x+r, y),
-    new PIXI.Point(x+offsetX, y+offsetY),
-    new PIXI.Point(x-offsetX, y+offsetY),
-    new PIXI.Point(x-r, y),
-    new PIXI.Point(x-offsetX, y-offsetY),
-    new PIXI.Point(x+offsetX, y-offsetY),
-    new PIXI.Point(x+r, y),
-  ];
+  startingTile.getSurroundingTiles().forEach(hex => stage.addChild(hex.shape));
 
-  const shape = new PIXI.Graphics()
-    .lineStyle(1, 0xFFFFFF)
-    .drawPolygon(points);
+  renderer.render(stage);
+};
 
-  shape.hitArea = new PIXI.Polygon(points);
-  shape.interactive = true;
-  shape.mouseover = () => {
-    shape.clear();
-    shape.beginFill(0x00FFFF);
-    shape.lineStyle(1, 0xFFFFFF)
-    shape.drawPolygon(points);
+startingTile.shape.mouseout = () => {
+  startingTile.shape.clear();
+  startingTile.shape.beginFill(0x000000);
+  startingTile.shape.lineStyle(1, 0xFFFFFF)
+  startingTile.shape.drawPolygon(startingTile.points);
 
-    renderer.render(stage);
-  };
-
-  shape.mouseout = () => {
-    shape.clear();
-    shape.beginFill(0x000000);
-    shape.lineStyle(1, 0xFFFFFF)
-    shape.drawPolygon(points);
-
-    renderer.render(stage);
-  }
-
-  return shape;
+  renderer.render(stage);
 }
-
-const startingPoint = getHex(width / 2, height / 2, 20, renderer, stage);
-stage.addChild(startingPoint);
---
+++ src/tile.js
@@ -0,0 +1,64 @@
+export default class Tile {
+
+  constructor(x, y, r, points, shape) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.points = points;
+    this.shape = shape;
+  }
+
+  getSurroundingTiles() {
+    // 30degrees = 0.5235988r
+    // Concept is the same as getting the points of the hexagon. length to side is required to do the vector calculation. Then it's the same.
+
+    // Length from centre of tile to the middle of a side.
+    const lengthToSide = Math.cos(0.5235988) * this.r;
+    const adjacentCentre1 = new PIXI.Point(this.x + ((lengthToSide * Math.cos(0.5235988)) * 2), this.y + ((lengthToSide * Math.sin(0.5235988)) * 2));
+    const adjacentCentre2 = new PIXI.Point(this.x + ((lengthToSide * Math.cos(1.570796)) * 2), this.y + ((lengthToSide * Math.sin(1.570796)) * 2));
+    const adjacentCentre3 = new PIXI.Point(this.x + ((lengthToSide * Math.cos(2.617994)) * 2), this.y + ((lengthToSide * Math.sin(2.617994)) * 2));
+    const adjacentCentre4 = new PIXI.Point(this.x - ((lengthToSide * Math.cos(0.5235988)) * 2), this.y - ((lengthToSide * Math.sin(0.5235988)) * 2));
+    const adjacentCentre5 = new PIXI.Point(this.x - ((lengthToSide * Math.cos(1.570796)) * 2), this.y - ((lengthToSide * Math.sin(1.570796)) * 2));
+    const adjacentCentre6 = new PIXI.Point(this.x - ((lengthToSide * Math.cos(2.617994)) * 2), this.y - ((lengthToSide * Math.sin(2.617994)) * 2));
+
+    return [
+      Tile.build(adjacentCentre1.x, adjacentCentre1.y, 20),
+      Tile.build(adjacentCentre2.x, adjacentCentre2.y, 20),
+      Tile.build(adjacentCentre3.x, adjacentCentre3.y, 20),
+      Tile.build(adjacentCentre4.x, adjacentCentre4.y, 20),
+      Tile.build(adjacentCentre5.x, adjacentCentre5.y, 20),
+      Tile.build(adjacentCentre6.x, adjacentCentre6.y, 20)
+    ];
+  }
+
+  // Draws a hexagon where (x, y) are the coordinates of the centre
+  // r is the radius of the hexagon.
+  // returns: shape Polygon object
+  static build(x, y, r) {
+    //60 degrees in radians = 1.04718
+    // OffsetX = length between center and a point (r) * cos(60 degrees)
+    // OffsetX = length between center and a point (r) * sin(60 degrees)
+    // This will produce the point at the bottom right of the hex (with 0,0 being at the top left of the canvas)
+    const offsetX = r * Math.cos(1.04718);
+    const offsetY = r * Math.sin(1.04718);
+
+    const points = [
+      new PIXI.Point(x+r, y),
+      new PIXI.Point(x+offsetX, y+offsetY),
+      new PIXI.Point(x-offsetX, y+offsetY),
+      new PIXI.Point(x-r, y),
+      new PIXI.Point(x-offsetX, y-offsetY),
+      new PIXI.Point(x+offsetX, y-offsetY),
+      new PIXI.Point(x+r, y),
+    ];
+
+    const shape = new PIXI.Graphics()
+      .lineStyle(1, 0xFFFFFF)
+      .drawPolygon(points);
+
+    shape.hitArea = new PIXI.Polygon(points);
+    shape.interactive = true;
+
+    return new Tile(x, y, r, points, shape);
+  }
+}
